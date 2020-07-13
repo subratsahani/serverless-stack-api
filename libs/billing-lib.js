7a81d1bbb48e480e4b88f5a96413ac8d7dfbeb83
backofficecomputingco <subrat.sahani@gmail.com>
2020-07-12 20:41:27 -0400

    Adding our Serverless API

--- libs/billing-lib.js
+++ libs/billing-lib.js
@@ -1,8 +1,10 @@
 export function calculateCost(storage) {
-    const rate = storage <= 10
-      ? 4
-      : storage <= 100
-        ? 2
-        : 1;
-    return rate * storage * 100;
+    // const rate = storage <= 10
+    //   ? 4
+    //   : storage <= 100
+    //     ? 2
+    //     : 1;
+    // return rate * storage * 100;
+    const rate = 5;
+    return rate * storage;
   }
